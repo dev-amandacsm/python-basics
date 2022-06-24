4cfe3405643eabf544745fb4a32c6dcd7ed28620
Amanda Muniz <amandacsm@ibm.com>
2022-06-23 21:00:07 -0300

    First coding lesson

---
+++ hello-world/__init__.py
@@ -0,0 +1,12 @@
+# we can use single quotes
+print('hello world!')
+
+# or double quotes...
+print("hello world!")
+
+''' ...but it's a good practice 
+to choose just one of the ways =) '''
+
+""" one more comment example,
+with double quotes """
+
